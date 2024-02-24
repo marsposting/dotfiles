2c25658c5f2ccd3ec16176f6a35e1154ffc07777
marsposting <code@bisnem.maskmy.id>
2024-02-24 15:38:40 -0800

    Add new tools and update dependencies

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,7 +1,9 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = 'Tokyo Night'
-config.font = wezterm.font('JetBrains Mono', { weight = 'Medium' })
+config.color_scheme = "Tokyo Night"
+config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
+config.initial_cols = 150
+config.initial_rows = 50
 
 return config
