f175792db9f7ff5716f6cf4b3597624d1066a727
marsposting <git@marsposting.io>
2024-05-06 16:12:58 -0700

    feat(neovim): project extras and neo-tree

---
+++ home/private_dot_config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      mappings = {
+        ["<Tab>"] = "toggle_node", -- map TAB to toggle expand/collapse
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
+    },
+  },
+}
