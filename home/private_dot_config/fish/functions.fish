cc791fa7223fd2bcc9df865128cbc6362580ec3e
marsposting <git@marsposting.io>
2024-05-06 17:21:25 -0700

    refactor: move configurable data back

--- home/private_dot_config/fish/functions.fish
+++
@@ -1,7 +0,0 @@
-function daily_refresh -d "Refresh Homebrew and packages"
-    brew update; and brew upgrade
-end
-
-function mcd -d "Make and change directories into"
-    mkdir -p $argv[1]; and cd $argv[1]
-end
